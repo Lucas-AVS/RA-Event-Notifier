94c5ca3d9742ba998231f633d710515d2f66846e
Lucas-AVS <lucas.alencar31@gmail.com>
2024-09-22 00:03:55 -0300

    feat: AchievBadge now uses dynamic API data

--- src/components/AchievBadge.tsx
+++ src/components/AchievBadge.tsx
@@ -1,44 +1,38 @@
-interface achievConfig {
-  achievement: {
-    id: number;
-    title: string;
-    description: string;
-    points: number;
-    trueRatio: number;
-    author: string;
-    dateCreated: string;
-    dateModified: string;
-    badgeName: string;
-    badgeUrl: string;
-  }}
+import './style/AchievBadge.css'
+import { Achievement } from '../assets/types';
+import './style/AchievBadge.css';
 
-const AchievementBadge = ({ achievement }:achievConfig) => {
-  return (
-    <div className="flex gap-x-2 mb-3 text-text">
-      <a href={`https://retroachievements.org/achievement/${achievement.id}`}>
-        <img
-          src={`https://media.retroachievements.org/Badge/${achievement.badgeName}.png`}
-          alt="Achievement of the week badge"
-          width="64"
-          height="64"
-          className="w-16 h-16"
-        />
-      </a>
+interface AchievBadgeProps {
+  achievement: Achievement;
+}
 
-      <div>
-        <a
-          href={`https://retroachievements.org/achievement/${achievement.id}`}
-          className="font-semibold leading-4 text-link"
-        >
-          {achievement.title}
+  const AchievementBadge = ({ achievement }:AchievBadgeProps) => {
+    return (
+      <div className="achievement-badge">
+        <a href={`https://retroachievements.org/achievement/${achievement.id}`}>
+          <img
+            src={`https://media.retroachievements.org/Badge/${achievement.badgeName}.png`}
+            alt="Achievement of the week badge"
+            className="achievement-badge-image"
+          />
         </a>
-        <p className="text-xs mb-1">
-          {achievement.points} <span className="TrueRatio">({achievement.trueRatio})</span> Points
-        </p>
-        <p className="text-xs">{achievement.description}</p>
+  
+        <div className="achievement-details">
+          <a
+            href={`https://retroachievements.org/achievement/${achievement.id}`}
+            className="achievement-title"
+          >
+            {achievement.title}
+          </a>
+          <p className="achievement-points">
+            {achievement.points} <span className="true-ratio">({achievement.trueRatio})</span> Points
+          </p>
+          <p className="achievement-description">{achievement.description}</p>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
+  
+
 
 export default AchievementBadge;
